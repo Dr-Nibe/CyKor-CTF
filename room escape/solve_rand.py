514aa1ecf1d5b1ecbee049e72e939b442e83852a
Dr-Nibe <59759771+Dr-Nibe@users.noreply.github.com>
2020-11-07 10:38:11 +0900

    Room Escape

--- room escape/solve_rand.py
+++ room escape/solve_rand.py
@@ -1,16 +1,21 @@
+# solve_rand.py
 from pwn import *
 from ctypes import *
 from ctypes.util import *
 
-r = process("./room_escape")
+r = remote("srv.cykor.kr", 31002)
 
 r.sendlineafter("> ", "1")
+
 r.recvuntil("> ")
 
 libc = CDLL(find_library('c'))
 seed = libc.time(0)
+print(seed)
+
 libc.srand(seed)
 ran = libc.rand() % 10000
+print(ran)
 
 r.sendline(str(ran).rjust(4, '0'))
 
